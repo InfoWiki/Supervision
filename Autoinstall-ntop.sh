297d53910fad77e96faf930f396ab261638fb72e
InfoWiki <contact@mickael-stanislas.com>
2012-12-30 18:49:45 +0100

    Auto Installation Ntop V1.0

---
+++ Autoinstall-ntop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# INSTALLATION AUTOMATISER DE NTOP - DEBIAN
+# Script dev. par Mickael Stanislas pour Infowiki
+# http://mickael-stanislas.com/
+# https://github.com/InfoWiki
+# http://infowiki.fr
+
+echo "" >> /etc/apt/sources.list
+echo "# SOURCE POUR NTOP" >> /etc/apt/sources.list
+echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list
+apt-get update
+apt-get upgrade -y 
+apt-get install -y ntop
+
+# ON RECUPERE L'ADRESSE IP DANS UNE VARIABLE
+ADDRIP=$(ifconfig eth0 | grep "inet addr" | cut -d " " -f 12 | cut -d : -f 2)
+
+clear
+echo
+echo "Installation terminé !"-n
+echo "Connectez vous" "http://""$ADDRIP"":3000" -n
+

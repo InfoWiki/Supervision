f4a6d2a50d6c3088ebee43d152a394b293d2a8d4
InfoWiki <contact@mickael-stanislas.com>
2012-12-30 20:06:24 +0100

    Auto Installation V1.06

--- ntop/Autoinstall-ntop.sh
+++ ntop/Autoinstall-ntop.sh
@@ -5,7 +5,7 @@
 # https://github.com/InfoWiki
 # http://infowiki.fr
 #
-# Version = 1.05
+# Version = 1.06
 # Synthaxe: bash ./Autoinstall-ntop.sh
 # Synthaxe: sudo bash ./Autoinstall-ntop.sh
 
@@ -15,7 +15,7 @@ echo "# SOURCE POUR NTOP" > /etc/apt/sources.list
 echo "deb http://backports.debian.org/debian-backports squeeze-backports main" > /etc/apt/sources.list
 # On fait les mises à jours
 apt-get update
-apt-get upgrade -y -f
+apt-get upgrade -y
 # On install Ntop
 apt-get install -y ntop
 # ON RECUPERE L'ADRESSE IP DANS UNE VARIABLE

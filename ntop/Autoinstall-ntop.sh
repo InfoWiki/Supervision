c0153766c161447c43df899cee42ba92b38c4191
InfoWiki <contact@mickael-stanislas.com>
2012-12-30 19:57:39 +0100

    Auto Installation V1.04

--- ntop/Autoinstall-ntop.sh
+++ ntop/Autoinstall-ntop.sh
@@ -4,11 +4,15 @@
 # http://mickael-stanislas.com/
 # https://github.com/InfoWiki
 # http://infowiki.fr
+#
+# Version = 1.04
+# Synthaxe: bash ./Autoinstall-ntop.sh
+# Synthaxe: sudo bash ./Autoinstall-ntop.sh
 
 echo "# SOURCE POUR NTOP" >> /etc/apt/sources.list
 echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list
 apt-get update
-apt-get upgrade -y 
+apt-get upgrade -y -f
 apt-get install -y ntop
 # ON RECUPERE L'ADRESSE IP DANS UNE VARIABLE
 ADDRIP=$(ifconfig eth0 | grep "inet addr" | cut -d " " -f 12 | cut -d : -f 2)
